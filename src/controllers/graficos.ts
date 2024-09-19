65095af88ec29f5dfdba6661dbe3a970ef929448
ljmdzst <ljmdzst@gmail.com>
2024-09-19 14:57:11 -0300

    add graficos/area

--- src/controllers/graficos.ts
+++ src/controllers/graficos.ts
@@ -3,13 +3,13 @@ import { response } from "express";
 import * as SGraficos from '../services/graficos'
 
 
-export const verGraficosDeAnio = async(req : any , resp : typeof response)=>{
+export const verGraficosGeneral = async(req : any , resp : typeof response)=>{
 
     try {
         
         const {anio } = req.params;
 
-        const graficos = await SGraficos.verGraficosDeAnio(anio);
+        const graficos = await SGraficos.verGraficosGeneral(Number(anio));
         
         resp.status(200).json({
             ok : true,
@@ -32,3 +32,35 @@ export const verGraficosDeAnio = async(req : any , resp : typeof response)=>{
     }
 
 }
+
+
+export const verGraficosDeArea = async(req : any , resp : typeof response)=>{
+
+    try {
+        
+        const {anio,idArea } = req.params;
+
+        const graficos = await SGraficos.verGraficosDeArea(Number(anio),Number(idArea));
+        
+        resp.status(200).json({
+            ok : true,
+            data : graficos,
+            error : null
+        });
+        
+        
+
+    } catch (error : any) {
+        
+        if( error.status && error.status === 500){
+            console.log( error.message );
+        }
+        resp.status( error.status || 500).json({
+            ok : false,
+            data : null,
+            error : error.message || 'Error de servidor'
+        })
+    }
+
+}
+
