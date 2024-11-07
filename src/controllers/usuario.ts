a3815bd765a02572d349c9f476aba345c11c3f4d
Carlos Jonatan Bordón <68975079+YonaBordon@users.noreply.github.com>
2024-11-07 17:11:00 -0300

    arreglo de email

--- src/controllers/usuario.ts
+++ src/controllers/usuario.ts
@@ -1,170 +1,162 @@
 import jwt from 'jsonwebtoken';
-import sequelizeExtension, { BD } from "../config/dbConfig";
-import {  Response, response } from 'express';
-import { Usuario } from "../models/Usuario"
-import { Persona} from '../models/Persona'
+import sequelizeExtension, { BD } from '../config/dbConfig';
+import { Response, response } from 'express';
+import { Usuario } from '../models/Usuario';
+import { Persona } from '../models/Persona';
 import { generarEmailValidaciónRegistro, smtpService } from '../config/smtpConfig';
 import { v4 as uuidv4 } from 'uuid';
 import { HttpHelpers } from '../helpers/general';
 import { PermisoAttributes } from '../models/Permiso';
 import { CategoriaAttributes } from '../models/Categoria';
 
+type DataListaUsuarios = { usuarios: { idUsuario: string; email: string }[] };
 
-type DataListaUsuarios = { usuarios :{ idUsuario : string , email : string }[] }
+export const loginUsuario = async (req: any, resp: typeof response) => {
+	try {
+		const { usuario, permisos, areas, categorias } = req.usuario;
 
-export const loginUsuario =  async( req : any, resp : typeof response  ) => {
+		const persona = await BD.Persona.findOne({
+			attributes: ['ape', 'nom'],
+			where: { nroDoc: usuario.nroDoc },
+		});
 
-    try {
-        const { usuario,permisos,areas,categorias } = req.usuario;
+		if (!persona)
+			throw { status: 500, message: `No existe persona asociada a usuario : ${usuario.idUsuario}` };
 
-        const persona = await BD.Persona.findOne({
-            attributes : ['ape','nom'],
-            where : { nroDoc : usuario.nroDoc }
-        })
+		const token = jwt.sign({ idUsuario: usuario.idUsuario }, process.env.HASH_KEY || '', {
+			expiresIn: '4h',
+		});
 
-        if( ! persona ) 
-            throw { status : 500 , message : `No existe persona asociada a usuario : ${usuario.idUsuario}`}
+		HttpHelpers.responderPeticionOk(resp, {
+			idUsuario: usuario.idUsuario,
+			email: usuario.email,
+			ape: persona.ape,
+			nom: persona.nom,
+			permisos: permisos.map((p: PermisoAttributes) => p.nombre),
+			categorias: categorias.map((c: CategoriaAttributes) => c.nombre),
+			areas: areas,
+			token: token,
+		});
+	} catch (error: any) {
+		HttpHelpers.responderPeticionError(resp, error.message || error, error.status || 500);
+	}
+};
 
-        const token = jwt.sign( {idUsuario : usuario.idUsuario} , process.env.HASH_KEY || '',{expiresIn : '4h'} )
+export const authUsuario = async (req: any, resp: typeof response) => {
+	try {
+		const { idUsuario } = req.usuario;
 
-        HttpHelpers.responderPeticionOk(resp, {
-            idUsuario : usuario.idUsuario,
-            email : usuario.email,
-            ape : persona.ape,
-            nom : persona.nom,
-            permisos : permisos.map((p : PermisoAttributes) => p.nombre),
-            categorias : categorias.map( (c : CategoriaAttributes) => c.nombre),
-            areas : areas,
-            token : token
-        });
-        
-    } catch (error : any) {
+		const _token = jwt.sign({ idUsuario }, process.env.HASH_KEY || '', { expiresIn: '4h' });
 
-        HttpHelpers.responderPeticionError(resp,error.message || error,error.status || 500)
-    }
+		HttpHelpers.responderPeticionOk(resp, { token: _token });
+	} catch (error: any) {
+		HttpHelpers.responderPeticionError(resp, error.status, error.message);
+	}
+};
 
-}
+export const registerUsuario = async (req: any, resp: typeof response) => {
+	try {
+		let salida = { respuesta: '' };
 
-export const authUsuario = async( req : any, resp : typeof response  )  => {
-   
-   try {
-        const {idUsuario}  = req.usuario;
-        
-        const _token = jwt.sign( {idUsuario} ,process.env.HASH_KEY || '', {expiresIn : '4h'} );
-        
-        HttpHelpers.responderPeticionOk(resp,{token : _token});
+		const { email, pass, idUnidadAcademica, confirmPass, ...dataPersona } = req.body;
 
-   } catch (error : any) {
-        HttpHelpers.responderPeticionError(resp,error.status,error.message);
-   }
+		const [dbPersona, creado] = await Persona.initModel(sequelizeExtension).findOrCreate({
+			defaults: { ...dataPersona, nroDoc: dataPersona.nroDoc },
+			where: { nroDoc: dataPersona.nroDoc },
+		});
 
-}
+		if (creado) {
+			const { tel, nroDoc, dom } = dataPersona;
+			dbPersona.set({ tel, nroDoc, dom });
+			await dbPersona.save();
+		}
 
-export  const registerUsuario = async( req : any, resp : typeof response  ) => {
+		const listaIdsUsados = await Usuario.verlistaIdsUsados(sequelizeExtension);
 
-    try {
-        let salida = { respuesta : ''};
-    
-        const {email,pass,idUnidadAcademica,confirmPass,...dataPersona} = req.body;
+		let nuevoId = uuidv4();
 
-        const [dbPersona,creado] = await Persona.initModel(sequelizeExtension).findOrCreate({
-            defaults : { ...dataPersona, nroDoc : dataPersona.nroDoc}, 
-            where : { nroDoc : dataPersona.nroDoc }
-        });
+		listaIdsUsados.forEach((usrId) => {
+			if (usrId === nuevoId) nuevoId = uuidv4();
+		});
 
-        if(creado){
-            const {tel,nroDoc,dom} = dataPersona;
-            dbPersona.set( {tel,nroDoc,dom});
-            await dbPersona.save();
-        }
+		const usuarioPendiente = await Usuario.initModel(sequelizeExtension).create({
+			idUsuario: nuevoId,
+			nroDoc: dbPersona.nroDoc,
+			idUnidadAcademica: idUnidadAcademica,
+			email,
+			pass,
+			pendiente: 1,
+		});
 
-        const listaIdsUsados = await Usuario.verlistaIdsUsados(sequelizeExtension);
-        
-        let nuevoId = uuidv4();
-        
-        listaIdsUsados.forEach( usrId => { if( usrId === nuevoId ) nuevoId = uuidv4() ;} )
-            
+		console.log('enviando correo confirmación..');
 
-        const usuarioPendiente = await Usuario.initModel(sequelizeExtension).create({
-            idUsuario : nuevoId,
-            nroDoc :dbPersona.nroDoc,
-            idUnidadAcademica : idUnidadAcademica,
-            email,
-            pass,
-            pendiente : 1
-        });
-        
+		const respSmtp = await smtpService.sendMail(
+			generarEmailValidaciónRegistro(usuarioPendiente.email, usuarioPendiente.idUsuario),
+		);
 
-        console.log('enviando correo confirmación..');
+		if (respSmtp.rejected.length > 0)
+			throw { status: 403, message: 'Envío de correo de confirmación rechazado' };
 
-        const respSmtp =  await smtpService.sendMail( generarEmailValidaciónRegistro( usuarioPendiente.email , usuarioPendiente.idUsuario) );
-    
-        if( respSmtp.rejected.length > 0 ) throw { status : 403 , message : 'Envío de correo de confirmación rechazado'}
-        
-        // Cambiar respuesta por HTML
-        salida = {respuesta : `Revise su casilla de correo ${respSmtp.accepted[0]}, y siga las instrucciones para completar el proceso de registro`}  
+		// Cambiar respuesta por HTML
+		salida = {
+			respuesta: `Revise su casilla de correo ${respSmtp.accepted[0]}, y siga las instrucciones para completar el proceso de registro`,
+		};
 
-        HttpHelpers.responderPeticionOk(resp, salida);
-    } catch (error : any) {
-        if(! error.status) console.log(error); 
-        HttpHelpers.responderPeticionError(resp,error.status, error.message);
-    }
-}
+		HttpHelpers.responderPeticionOk(resp, salida);
+	} catch (error: any) {
+		if (!error.status) console.log(error);
+		HttpHelpers.responderPeticionError(resp, error.status, error.message);
+	}
+};
 
+export const validarRegistro = async (req: any, resp: typeof response) => {
+	try {
+		const usuarioPendiente = await BD.Usuario.findByPk(req.params.idUsuario);
 
+		if (!usuarioPendiente) throw { status: 400, msg: 'No existe usuario con ese id' };
 
-export const validarRegistro = async (req : any , resp : typeof response) => {
+		usuarioPendiente.set('pendiente', 0);
 
-  try {
-    const usuarioPendiente =  await BD.Usuario.findByPk( req.params.idUsuario  );
+		await usuarioPendiente.save();
 
-    if( ! usuarioPendiente ) throw { status : 400, msg : 'No existe usuario con ese id' }
- 
-    usuarioPendiente.set('pendiente',0);
-    
-    await usuarioPendiente.save();
-    
-   HttpHelpers.responderPeticionOk(resp,{
-        respuesta : `Usuario ${usuarioPendiente.email} registrado !`
-   });
-  } catch (error : any) {
-     if(!error.status) { console.log(error); }
-     HttpHelpers.responderPeticionError(resp,error.status, error.message);
-  }
+		HttpHelpers.responderPeticionOk(resp, {
+			respuesta: `Usuario ${usuarioPendiente.email} registrado !`,
+		});
+	} catch (error: any) {
+		if (!error.status) {
+			console.log(error);
+		}
+		HttpHelpers.responderPeticionError(resp, error.status, error.message);
+	}
+};
 
+export const verListaUsuarios = async (req: any, resp: typeof response) => {
+	try {
+		let salida: DataListaUsuarios = { usuarios: [] };
 
-}
+		const usuariosEnBD = await Usuario.initModel(sequelizeExtension).findAll({
+			attributes: ['idUsuario', 'email'],
+		});
 
-export const verListaUsuarios =  async (req : any , resp : typeof response) => {
-    try {
-        let salida : DataListaUsuarios = { usuarios : [] }
+		salida.usuarios = usuariosEnBD.map((usr) => usr.dataValues);
 
-
-        const usuariosEnBD = await Usuario.initModel(sequelizeExtension).findAll( { 
-            attributes : ['idUsuario','email']
-        } );
-
-        salida.usuarios = usuariosEnBD.map( usr => usr.dataValues)
-
-        HttpHelpers.responderPeticionOk(resp,salida);
-    } catch (error : any) {
-        HttpHelpers.responderPeticionError(resp,error.status, error.message)
-    }
-}
+		HttpHelpers.responderPeticionOk(resp, salida);
+	} catch (error: any) {
+		HttpHelpers.responderPeticionError(resp, error.status, error.message);
+	}
+};
 
 export const getDataUsuarioPorId = async (req: any, res: Response) => {
 	try {
 		const { idUsuario } = req.params;
 
-		console.log(idUsuario);
-
 		const persona = await BD.Persona.findOne({
 			include: [
 				{
 					model: BD.Usuario,
-					as: 'Usuarios', 
+					as: 'Usuarios',
 					where: { idUsuario },
-					attributes: [], 
+					attributes: ['email'], // Retrieve the email field only
 				},
 			],
 			attributes: {
@@ -179,7 +171,13 @@ export const getDataUsuarioPorId = async (req: any, res: Response) => {
 			};
 		}
 
-		HttpHelpers.responderPeticionOk(res, persona);
+		const personaData = persona.toJSON() as any;
+
+		personaData.email = personaData.Usuarios?.[0]?.email || null;
+
+		delete personaData.Usuarios;
+
+		HttpHelpers.responderPeticionOk(res, personaData);
 	} catch (error: any) {
 		HttpHelpers.responderPeticionError(
 			res,
@@ -192,52 +190,52 @@ export const getDataUsuarioPorId = async (req: any, res: Response) => {
 
 
 export const updateDataUsuarioPorId = async (req: any, res: Response) => {
-    try {
-        const { idUsuario } = req.params;
-        let updatedData = req.body;
+	try {
+		const { idUsuario } = req.params;
+		let updatedData = req.body;
 
-        const restricciones = [];
-        if (updatedData.dni) {
-            restricciones.push('dni');
-            delete updatedData.dni;
-        }
-        if (updatedData.email) {
-            restricciones.push('email');
-            delete updatedData.email;
-        }
+		const restricciones = [];
+		if (updatedData.dni) {
+			restricciones.push('dni');
+			delete updatedData.dni;
+		}
+		if (updatedData.email) {
+			restricciones.push('email');
+			delete updatedData.email;
+		}
 
-        const persona = await BD.Persona.findOne({
-            include: [
-                {
-                    model: BD.Usuario,
-                    as: 'Usuarios',
-                    where: { idUsuario },
-                    attributes: [],
-                },
-            ],
-        });
+		const persona = await BD.Persona.findOne({
+			include: [
+				{
+					model: BD.Usuario,
+					as: 'Usuarios',
+					where: { idUsuario },
+					attributes: [],
+				},
+			],
+		});
 
-        if (!persona) {
-            throw {
-                status: 404,
-                message: `No existe persona asociada al usuario con ID: ${idUsuario}`,
-            };
-        }
+		if (!persona) {
+			throw {
+				status: 404,
+				message: `No existe persona asociada al usuario con ID: ${idUsuario}`,
+			};
+		}
 
-        await persona.update(updatedData);
-        res.json({
-            ok: true,
-            data: persona,
-            advertencia: restricciones.length
-                ? `Los campos ${restricciones.join(", ")} no pueden ser modificados.`
-                : null,
-            error: null,
-        });
-    } catch (error: any) {
-        HttpHelpers.responderPeticionError(
-            res,
-            error.status || 500,
-            error.message || 'Error inesperado',
-        );
-    }
+		await persona.update(updatedData);
+		res.json({
+			ok: true,
+			data: persona,
+			advertencia: restricciones.length
+				? `Los campos ${restricciones.join(', ')} no pueden ser modificados.`
+				: null,
+			error: null,
+		});
+	} catch (error: any) {
+		HttpHelpers.responderPeticionError(
+			res,
+			error.status || 500,
+			error.message || 'Error inesperado',
+		);
+	}
 };
